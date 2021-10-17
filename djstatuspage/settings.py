6e7f5878728ab25fa90afc02bc68ecb6f6dcd437
GorlikItsMe <33355238+GorlikItsMe@users.noreply.github.com>
2021-10-17 18:34:58 +0200

    load config from .env

--- djstatuspage/settings.py
+++ djstatuspage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wb@upjo^y6$)i8)jy5g6ce!zk@^vitnjgwaj3@l@+&-+p_)6!q'
+SECRET_KEY = environ.get('SECRET_KEY', 'django-insecure-wb@upjo^y6$)i8)jy5g6ce!zk@^vitnjgwaj3@l@+&-+p_)6!q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS.append(environ.get('ALLOWED_HOSTS', '127.0.0.2'))
 
 
 # Application definition
@@ -100,11 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+SESSION_COOKIE_SECURE=True
+CSRF_COOKIE_SECURE=True
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = environ.get('LANGUAGE_CODE', 'en-us')
 
 TIME_ZONE = 'UTC'
 
