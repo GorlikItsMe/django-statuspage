765e17ea02bbac1378e7a67823bcc5bbc7e4f0bc
GorlikItsMe <33355238+GorlikItsMe@users.noreply.github.com>
2021-10-18 19:15:52 +0200

    Listing Services

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Service
 
 
-class StatusPageView(TemplateView):
+class StatusPageView(ListView):
     template_name = 'statuspage.html'
+    queryset = Service.objects.all().order_by('pos')
